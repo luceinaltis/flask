e6faba30864f8e6bace1e1d8246782384732152f
luceinaltis <luceinaltis2020@naver.com>
2020-12-20 17:58:09 +0900

    update

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
 
 app = Flask("SuperScrapper")
 
-app.run(host="0.0.0.0")
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/report")
+def report():
+    query_job = request.args.get("query_job")
+    if query_job:
+        query_job = query_job.lower()
+    else:
+        return redirect("/")
+    return render_template("report.html", searchingBy=query_job)
+
+
+app.run(host="127.0.0.1")
