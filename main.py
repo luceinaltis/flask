881f3fd3873daeacbcb8533c547d2e380ed9b2f6
luceinaltis <luceinaltis2020@naver.com>
2020-12-20 20:18:58 +0900

    update

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, request, redirect
-
+from flask import Flask, render_template, request, redirect, send_file
+from scraper import get_jobs
+from exporter import save_to_file
 
 app = Flask("SuperScrapper")
+
+db = {}
 
 
 @app.route("/")
@@ -14,9 +17,31 @@ def report():
     query_job = request.args.get("query_job")
     if query_job:
         query_job = query_job.lower()
+        if db.get(query_job):
+            jobs = db.get(query_job)
+        else:
+            jobs = get_jobs(query_job)
+            db[query_job] = jobs
+
     else:
         return redirect("/")
-    return render_template("report.html", searchingBy=query_job)
+    return render_template("report.html", searchingBy=query_job, searchingNum=len(jobs), jobs=jobs)
+
+
+@app.route("/export")
+def export():
+    try:
+        word = request.args.get("query_job")
+        if not word:
+            raise Exception()
+        word = word.lower()
+        jobs = db.get(word)
+        if not jobs:
+            raise Exception()
+        save_to_file(jobs)
+        return send_file("jobs.csv")
+    except:
+        return redirect("/")
 
 
 app.run(host="127.0.0.1")
---
+++ scraper.py
@@ -0,0 +1,44 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_last_page(url):
+    result = requests.get(url)
+    soup = BeautifulSoup(result.text, "html.parser")
+    pagination = soup.find("div", {"class": "s-pagination"}).find_all("a")
+    last_page = pagination[-2].get_text(strip=True)
+
+    return int(last_page)
+
+
+def extract_job(html):
+    title = html.find("a", {"class": "s-link stretched-link"})["title"]
+    company, location = html.find(
+        "h3", {"class": "fc-black-700 fs-body1 mb4"}).find_all("span", recursive=False)
+    company = company.get_text(strip=True)
+    location = location.get_text(strip=True)
+    job_id = html["data-jobid"]
+    return {'title': title, "company": company, "location": location, "link": f"https://stackoverflow.com/jobs/{job_id}"}
+
+
+def extract_jobs(last_page, url):
+    jobs = []
+    for page in range(last_page):
+        print(f"StackOverflow 긁어오는중 {page} of {last_page}")
+        result = requests.get(f"{url}&pg={page+1}")
+        soup = BeautifulSoup(result.text, "html.parser")
+
+        results = soup.find_all("div", {"class": "-job"})
+        for result in results:
+            job = extract_job(result)
+            jobs.append(job)
+
+    return jobs
+
+
+def get_jobs(word):
+    url = f"https://stackoverflow.com/jobs?q={word}&sort=i"
+    last_page = get_last_page(url)
+    jobs = extract_jobs(last_page, url)
+
+    return jobs
